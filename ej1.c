c3e1c0eda2b03d1967228cbb4db2be8bdbcbc8f2
francolop <franco.gabriel.lopez@mi.unc.edu.ar>
2024-08-11 18:35:31 -0300

    Add files via upload

---
+++ ej1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    
+    int x;
+    int y = 5;
+    int suma = 0;
+    int resta = 0;
+    int mult = 0;
+    int div = 0;
+
+    x = 150;
+
+    suma = x + y;
+    resta = x - y;
+    mult = x * y;
+    div = x / y;
+    printf("La suma es: %d\n",suma);
+    printf("La resta es: %d\n",resta);
+    printf("El producto es: %d\n",mult);
+    printf("La division es: %d\n",div);
+    printf("Adios mundo");
+
+
+    return 0;
+}
+
+
+    //comentario
+    /*\t tab \n newline "\cadena de caracteres con comillas\"*/
